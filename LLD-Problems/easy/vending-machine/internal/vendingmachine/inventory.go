ce503b647b30a9a068e33e15e4da19eff251957b
Ashish Jaiswal <Ashish.Jaiswal3@ibm.com>
2025-04-28 22:14:12 +0530

    added vending machine problem for LLD

---
+++ LLD-Problems/easy/vending-machine/internal/vendingmachine/inventory.go
@@ -0,0 +1,18 @@
+package vendingmachine
+
+type Inventory struct {
+	products map[*Product]int
+}
+
+func NewInventory() *Inventory {
+	return &Inventory{products: make(map[*Product]int)}
+}
+
+func (inv *Inventory) AddProduct(product *Product, quantity int) {
+	inv.products[product] = quantity
+}
+
+func (inv *Inventory) IsAvailable(product *Product) bool {
+	qty, exists := inv.products[product]
+	return exists && qty > 0
+}
